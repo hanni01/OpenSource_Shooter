5623d5937251a747d84f465fe559f95efcf45afa
김민기 <kai030725@daum.net>
2025-05-25 11:20:40 +0900

    feat: 전체 파일 기능 일괄 커밋

---
+++ Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyUI : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("Level_00");
+    }
+}
